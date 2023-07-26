b4555352c920cd8ce281451bbe9440866a171e86
mrriina <mariaip12345678911@gmail.com>
2023-07-26 14:00:10 +0300

    generating columns array for datatable

--- force-app/main/default/lwc/showDatatable/showDatatable.js
+++ force-app/main/default/lwc/showDatatable/showDatatable.js
@@ -1,12 +1,39 @@
-import { LightningElement, api } from 'lwc';
-import { CurrentPageReference } from 'lightning/navigation';
+import { LightningElement, api, wire } from 'lwc';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 
 export default class ShowDatatable extends LightningElement {
     @api checkedFieldNames;
     @api selectedObjectName;
+    columns = [];
+    data = [];
+
+    @wire(getObjectInfo, { objectApiName: '$selectedObjectName' })
+    objectInfo;
 
     connectedCallback() {
         this.checkedFieldNames = JSON.parse(this.checkedFieldNames);
-        console.log('checkedFieldNames[1]='+this.checkedFieldNames[1]);
+    }
+
+    get columnsInfo() {
+        if (this.objectInfo.data) {
+            this.generateColumns();
+        }
+        return this.columns;
+    }
+
+
+    generateColumns() {
+        if (this.objectInfo.data) {
+            const fields = this.objectInfo.data.fields;
+            this.checkedFieldNames.forEach((fieldName) => {
+                const field = fields[fieldName];
+                if (field) {
+                    this.columns.push({
+                        label: field.label,
+                        fieldName: field.apiName
+                    });
+                }
+            });
+        }
     }
 }
