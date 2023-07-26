72ad93ca0ae3999fdd6e75320ae221e6158a3cfe
mrriina <mariaip12345678911@gmail.com>
2023-07-26 17:32:30 +0300

    retrieve records for datatable

--- force-app/main/default/lwc/showDatatable/showDatatable.js
+++ force-app/main/default/lwc/showDatatable/showDatatable.js
@@ -1,17 +1,35 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { CurrentPageReference } from 'lightning/navigation';
+import fetchData from '@salesforce/apex/DatatableConroller.fetchData';
 
 export default class ShowDatatable extends LightningElement {
     @api checkedFieldNames;
     @api selectedObjectName;
     columns = [];
     data = [];
+    recordId;
 
     @wire(getObjectInfo, { objectApiName: '$selectedObjectName' })
     objectInfo;
 
+    @wire(CurrentPageReference) pageRef;
+
     connectedCallback() {
         this.checkedFieldNames = JSON.parse(this.checkedFieldNames);
+        this.recordId = this.pageRef.attributes.recordId;
+        
+    }
+
+    @wire(fetchData, {fieldNames: '$checkedFieldNames', objectApiName:'$selectedObjectName', recordId: '$recordId'})
+    getDataForDatatable({data, error}){ 
+        if(data){ 
+            console.log('data==', JSON.stringify(data));
+            this.data = data;
+        }
+        if(error){ 
+            console.log(error);
+        }
     }
 
     get columnsInfo() {
